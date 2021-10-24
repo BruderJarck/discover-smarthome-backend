685f9d8da961b2a59c0c17ce6f5a951400f2f057
butilka123 <alexgaming1575@gmail.com>
2021-10-24 23:01:01 +0200

    add search-bar to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './components/cart/cart.component';
-import { NavComponent } from './components/nav/nav.component';
+import { Login, NavComponent } from './components/nav/nav.component';
 import { ShopComponent } from './components/shop/shop.component';
 import components from './pages';
 import { SharedModule } from './shared/shared-module';
@@ -16,6 +16,7 @@ import { SharedModule } from './shared/shared-module';
     ShopComponent,
     CartComponent,
     NavComponent,
+    Login,
   ],
   imports: [
     BrowserModule,
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
+export interface Chip {
+  name: string;
+}
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openDialog() {
+    const dialogRef = this.dialog.open(Login);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
+}
 
+@Component({
+  selector: 'login',
+  templateUrl: 'login.html',
+})
+export class Login {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
