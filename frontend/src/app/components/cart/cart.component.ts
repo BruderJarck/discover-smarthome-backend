5c3063ec3ff5228df0ef6859ea4ec057368dcf5a
butilka123 <66038983+butilka123@users.noreply.github.com>
2021-12-07 12:02:35 +0100

    add summary logic

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -8,12 +8,21 @@ import { SharedService } from 'src/app/shared/shared.service';
 })
 export class CartComponent implements OnInit {
   items: any = [];
+  
+  totalPrice: number = 0
+  mwst: number = 0.24 //in %
+  workCost: number = 120 //in €
+  totalItems: number = 0
+  totalItemsBaseCost: number = 0
+  shipCost: number = 78 //in €
 
   constructor(public sharedService: SharedService) {}
 
   ngOnInit(): void {
     this.sharedService.productList.subscribe((item: any) => {
       this.items = [...item];
+
+      this.calcPrice();
     });
   }
 
@@ -31,5 +40,18 @@ export class CartComponent implements OnInit {
     } else {
       this.sharedService.changeAmmountById(id, -1);
     }
+  }
+
+  calcPrice() {
+    this.totalItems = 0
+    this.totalPrice = 0
+    this.totalItemsBaseCost = 0
+
+    for (let item of this.items) { 
+      this.totalItemsBaseCost = this.totalItemsBaseCost + (item.ammount * ( item.product.price + 0.99))
+      this.totalItems = this.totalItems + item.ammount
+    }
+    
+    this.totalPrice = this.totalItemsBaseCost + (this.mwst * this.totalItemsBaseCost) + this.workCost + (this.shipCost * this.totalItems)
   }
 }
