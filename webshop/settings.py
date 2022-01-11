042970f30795e325325e65a294b697f1e0beb724
jackwolf24 <ajaygaly@gmail.com>
2022-01-11 01:47:09 +0100

    update

--- webshop/settings.py
+++ webshop/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/app/media')
+MEDIA_URL = '/app/media/'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10000000
 
 REST_FRAMEWORK = {
